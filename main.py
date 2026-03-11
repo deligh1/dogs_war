674df28cbf8ed525ea5ff582c9f904b70c3477e9
lazclendindla <134687589+lazclendindla@users.noreply.github.com>
2026-03-12 01:04:20 +0900

    meppoitsuyoi bug naoshita tabun

--- main.py
+++ main.py
@@ -169,8 +169,8 @@ class Game:
         elif item["effect"] == "castle_hp_up":
             self.data2["items"]["castle_hp"] *= item["value"]
         elif item["effect"] == "meppo":
-            if "meppo" in [i["name"] for i in self.data2["allies"][item["value"][0]]["param"][9]]:
-                self.data2["allies"][item["value"][0]]["params"][9]["meppo"]["value"].append(item["value"][1])
+            if "meppo" in [i["name"] for i in self.data2["allies"][item["value"][0]]["status"][9]]:
+                self.data2["allies"][item["value"][0]]["status"][9]["meppo"]["value"].append(item["value"][1])
             else:
                 self.data2["allies"][item["value"][0]]["params"][9]["meppo"] = {"value": [item["value"][1]]}
         else:
